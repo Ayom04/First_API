b2a60fc64784c6b1deac28b71023218754454904
Ayom04 <abdullahayomide04@gmail.com>
2023-05-15 16:45:18 +0100

    working with devdependencies

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  res.setHeader('Content-Type', 'application/json');
+  res.write(JSON.stringify({
+    message: "application"
+  }))
+  res.end();
 });
 
 server.listen(port, hostname, () => {
